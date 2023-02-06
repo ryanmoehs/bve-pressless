7f208e4ff9c76e12c7bceae48fc0fb3a45c62c7e
ryanmoehs <masryandepok@gmail.com>
2023-02-06 10:15:01 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# BVE-Pressless
+Play BVE Train Simulator with camera <br>
+Inspired by : https://youtu.be/HgFxFOzxCPk
+
+> <b>Disclaimer!</b><br>
+> I use BVE 5. I don't know this program can be use in another version or not
+
+## Library
+- `opencv`
+- `pyautogui`
+- `cvzone` (You must install `mediapipe` and `opencv` first)
+
+## BVE Settings
+`Preferences... > Input Devices > Key assignments...`
+<div align="center">
+    <img src="img/key/bve-key-rev.png" width=250>
+    <img src="img/key/bve-key-p.png" width=250>
+    <img src="img/key/bve-key-b.png" width=250>
+    <img src="img/key/bve-key-p-b.png" width=250>
+</div>
+
+## Key
+Based on BVE settings, you can use : 
+- <b>Clench</b> : Neutral <br>
+<img src="img/key/neutral.png" width=150>
+- <b>Pointing ahead</b> : Move reverser Forward // Ubah reverser ke "Maju"<br>
+<img src="img/key/forward.png" width=150>
+- <b>Pointing backward (with thumb)</b> : Move reverser Backward // Ubah reverser ke "Mundur"<br>
+<img src="img/key/back.png" width=150>
+- <b>Index up</b> : Reduce Power // Kurangi Tenaga <br>
+<img src="img/key/red-t.png" width=150>
+- <b>Index with Middle</b> : Increase Power // Tambah Tenaga <br>
+<img src="img/key/inc-t.png" width=150>
+- <b>Index, Middle, and Ring</b> : Reduce Brake // Kurangi Rem <br>
+<img src="img/key/red-b.png" width=150>
+- <b>Index, Middle, Ring, and Little</b> : Increase Brake // Tambah Rem<br>
+<img src="img/key/inc-b.png" width=150>
+- <b>Splayed hand</b> : Emergency Brake // Rem Darurat<br>
+<img src="img/key/eb.png" width=150>
+- <b>"Call"</b> : Horn // Klakson<br>
+<img src="img/key/horn.png" width=150>
+
+> You must move fast if you just want to move one step e.g. brake <br>
+> Harus gercep kalau hanya ingin bergerak satu langkah, misal rem
---
+++ app.py
@@ -0,0 +1,81 @@
+import cv2
+import pyautogui
+from cvzone.HandTrackingModule import HandDetector
+
+hand_detector = HandDetector(detectionCon=0.8, maxHands=2)
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    hands, img = hand_detector.findHands(frame)
+    if hands:
+        lmlist = hands[0]
+        fingerUp = hand_detector.fingersUp(lmlist)
+        # print(fingerUp) # buat nyari tau jumlah jari yang diangkat
+
+        # styling
+
+        # color : (B,G,R)
+        blue = (255,0,0)
+        green = (0,200,0)
+        red = (0,0,255)
+        yellow = (0,255,200)
+
+        font = cv2.FONT_HERSHEY_DUPLEX
+        position = (30,80)
+
+        # clench // kepal
+        # if fingerUp == [0, 0, 0, 0, 0] or fingerUp == [1,0,1,1,1]: # clench or "0"-style // kepal atau angka 0 
+        if fingerUp == [0, 0, 0, 0, 0]:
+            cv2.putText(frame, "Neutral", position, font, 1, yellow, 1, cv2.LINE_AA)
+            pyautogui.press('s')
+
+        # pointing ahead // nunjuk ke depan
+        elif fingerUp == [1,1,0,0,0]:
+            cv2.putText(frame, "Forward", position, font, 1, blue, 1, cv2.LINE_AA)
+            pyautogui.press('up')
+
+        # pointing backward (with thumb) // nunjuk ke belakang (pake jempol)
+        elif fingerUp == [1,0,0,0,0]:
+            cv2.putText(frame, "Backward", position, font, 1, blue, 1, cv2.LINE_AA)
+            pyautogui.press('down')
+
+        # index up // telunjuk ke atas
+        elif fingerUp == [0, 1, 0, 0, 0]:
+            cv2.putText(frame, "Reduce throttle ", position, font, 1, green, 1, cv2.LINE_AA)
+            pyautogui.press('a')
+
+        # index with middle // telunjuk dengan jari tengah
+        elif fingerUp == [0, 1, 1, 0, 0]:
+            cv2.putText(frame, "Increase throttle", position, font, 1, green, 1, cv2.LINE_AA)
+            pyautogui.press('z')
+
+        # index, middle, and ring // telunjuk, jari tengah, dan jari manis
+        elif fingerUp == [0, 1, 1, 1, 0]:
+            cv2.putText(frame, "Reduce Brake", position, font, 1, red, 1, cv2.LINE_AA)
+            pyautogui.press(',')
+
+        # index, middle, ring, and little // telunjuk, jari tengah, jari manis, dan kelingking
+        elif fingerUp == [0, 1, 1, 1, 1]:
+            cv2.putText(frame, "Increase Brake", position, font, 1, red, 1, cv2.LINE_AA)
+            pyautogui.press('.')
+
+        # all finger (5 figer) // semua jari (5 jari)
+        elif fingerUp == [1, 1, 1, 1, 1]:
+            cv2.putText(frame, "Emergency Brake", position, font, 1, red, 1, cv2.LINE_AA)
+            pyautogui.press('/')
+
+        # "call" // "telpon"
+        elif fingerUp == [0, 0, 0, 0, 1]:
+            cv2.putText(frame, "Horn", position, font, 1, red, 1, cv2.LINE_AA)
+            pyautogui.press('enter')
+            
+    cv2.imshow("BVE-Pressless", frame)
+    k = cv2.waitKey(1)
+
+    # close with "X" button // menutup pake tombol "X"
+    if cv2.getWindowProperty('BVE-Pressless',cv2.WND_PROP_VISIBLE) < 1:
+        break
+
+cap.release()
+cv2.destroyAllWindows() 
